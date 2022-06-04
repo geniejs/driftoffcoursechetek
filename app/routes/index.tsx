4582bb8c58d1c1449c59e1c016c002b10a43b94a
Corey Jepperson <corey@entropy.cc>
2022-06-04 00:31:42 -0500

    mabye

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,8 +11,8 @@ import { getDB } from "~/lib/db.server";
 export let loader: LoaderFunction = async ({ context, request }) => {
   const url = new URL(request.url);
   let db: PrismaClient | undefined = undefined;
-  if (url.searchParams.has("dynamic")) {
-    //db = getDB();
+  if (url.searchParams.has("dynamic") && Date.now() > 1654324250000) {
+    db = getDB();
   }
   const posts = await getPosts(db);
   const homePosts = posts.filter((post) => post.page === "home");
